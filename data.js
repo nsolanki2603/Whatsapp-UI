26658bf9940f2b9a60284470a2c52fb1417e0a64
nsolanki2603 <51415714+nsolanki2603@users.noreply.github.com>
2020-02-14 17:51:12 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+[
+	{
+		id: "01",
+		name: "Geetika"
+	},
+	{
+		id: "02",
+		name: "Nayan"
+	},
+	{
+		id: "03",
+		name: "Digvijay"
+	},
+	{
+		id: "04",
+		name: "Ketan"
+	},
+	{
+		id: "05",
+		name: "Deekshit"
+	},
+	{
+		id: "06",
+		name: "Veenu"
+	},
+	{
+		id: "07",
+		name: "Busaina"
+	
+},
+	{
+		id: "08",
+		name: "Purvi"
+},
+	{
+		id: "09",
+		name: "Bhupi"
+}
+]
